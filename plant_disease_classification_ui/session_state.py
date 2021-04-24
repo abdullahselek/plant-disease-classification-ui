fc519dfdc00d9406dcaf4ecc951b0379f524e85a
Abdullah Selek <abdullahselek@gmail.com>
2021-04-24 23:21:03 +0100

    Indentation fix session state

--- plant_disease_classification_ui/session_state.py
+++ plant_disease_classification_ui/session_state.py
@@ -45,67 +45,67 @@ class SessionState(object):
             setattr(self, key, val)
 
 
-def get(**kwargs):
-    """Gets a SessionState object for the current session.
-    Creates a new object if necessary.
-    Parameters
-    ----------
-    **kwargs : any
-        Default values you want to add to the session state, if we're creating a
-        new one.
-    Example
-    -------
-    >>> session_state = get(user_name='', favorite_color='black')
-    >>> session_state.user_name
-    ''
-    >>> session_state.user_name = 'Mary'
-    >>> session_state.favorite_color
-    'black'
-    Since you set user_name above, next time your script runs this will be the
-    result:
-    >>> session_state = get(user_name='', favorite_color='black')
-    >>> session_state.user_name
-    'Mary'
-    """
-    # Hack to get the session object from Streamlit.
+    def get(**kwargs):
+        """Gets a SessionState object for the current session.
+        Creates a new object if necessary.
+        Parameters
+        ----------
+        **kwargs : any
+            Default values you want to add to the session state, if we're creating a
+            new one.
+        Example
+        -------
+        >>> session_state = get(user_name='', favorite_color='black')
+        >>> session_state.user_name
+        ''
+        >>> session_state.user_name = 'Mary'
+        >>> session_state.favorite_color
+        'black'
+        Since you set user_name above, next time your script runs this will be the
+        result:
+        >>> session_state = get(user_name='', favorite_color='black')
+        >>> session_state.user_name
+        'Mary'
+        """
+        # Hack to get the session object from Streamlit.
 
-    ctx = ReportThread.get_report_ctx()
+        ctx = ReportThread.get_report_ctx()
 
-    this_session = None
+        this_session = None
 
-    current_server = Server.get_current()
-    if hasattr(current_server, "_session_infos"):
-        # Streamlit < 0.56
-        session_infos = Server.get_current()._session_infos.values()
-    else:
-        session_infos = Server.get_current()._session_info_by_id.values()
+        current_server = Server.get_current()
+        if hasattr(current_server, "_session_infos"):
+            # Streamlit < 0.56
+            session_infos = Server.get_current()._session_infos.values()
+        else:
+            session_infos = Server.get_current()._session_info_by_id.values()
 
-    for session_info in session_infos:
-        s = session_info.session
-        if (
-            # Streamlit < 0.54.0
-            (hasattr(s, "_main_dg") and s._main_dg == ctx.main_dg)
-            or
-            # Streamlit >= 0.54.0
-            (not hasattr(s, "_main_dg") and s.enqueue == ctx.enqueue)
-            or
-            # Streamlit >= 0.65.2
-            (
-                not hasattr(s, "_main_dg")
-                and s._uploaded_file_mgr == ctx.uploaded_file_mgr
+        for session_info in session_infos:
+            s = session_info.session
+            if (
+                # Streamlit < 0.54.0
+                (hasattr(s, "_main_dg") and s._main_dg == ctx.main_dg)
+                or
+                # Streamlit >= 0.54.0
+                (not hasattr(s, "_main_dg") and s.enqueue == ctx.enqueue)
+                or
+                # Streamlit >= 0.65.2
+                (
+                    not hasattr(s, "_main_dg")
+                    and s._uploaded_file_mgr == ctx.uploaded_file_mgr
+                )
+            ):
+                this_session = s
+
+        if this_session is None:
+            raise RuntimeError(
+                "Oh noes. Couldn't get your Streamlit Session object. "
+                "Are you doing something fancy with threads?"
             )
-        ):
-            this_session = s
 
-    if this_session is None:
-        raise RuntimeError(
-            "Oh noes. Couldn't get your Streamlit Session object. "
-            "Are you doing something fancy with threads?"
-        )
+        # Got the session object! Now let's attach some state into it.
 
-    # Got the session object! Now let's attach some state into it.
+        if not hasattr(this_session, "_custom_session_state"):
+            this_session._custom_session_state = SessionState(**kwargs)
 
-    if not hasattr(this_session, "_custom_session_state"):
-        this_session._custom_session_state = SessionState(**kwargs)
-
-    return this_session._custom_session_state
+        return this_session._custom_session_state
